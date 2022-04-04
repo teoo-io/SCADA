f0dfc040e555eceb8c28064968c430ede16c11bd
Mateo Ortegon <m@teoo.io>
2022-04-04 00:49:47 -0600

    progress

--- src/user_create_handler.php
+++ src/user_create_handler.php
@@ -2,7 +2,6 @@
     session_start();
     $email = $_POST['email'];
     $password = $_POST['password'];
-    echo "<pre>" . print_r($_POST) . "</pre>";
     require_once 'dao.php';
     $dao = new DAO();
     $dao->createUser($email, $password,1);
