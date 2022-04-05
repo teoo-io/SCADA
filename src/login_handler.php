19d55afd5db11e8a5ac9051a06f93cd9191f0b82
Mateo Ortegon <m@teoo.io>
2022-04-04 23:16:29 -0600

    progress

--- src/login_handler.php
+++ src/login_handler.php
@@ -7,7 +7,7 @@
     $errors = array();
     $_SESSION['authenticated'] = $dao->userExists($email, $password);
 
-    if($_SESSION['authenticated']){
+    if($_SESSION['authenticated']==1){
         header('Location: index.php');
     } else {
         $errors[] = "The user or password you entered is not valid.";
