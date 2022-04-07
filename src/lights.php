fb98cee1a8828e30e5953768b7e0ce63b8314080
Mateo Ortegon <m@teoo.io>
2022-04-07 03:26:04 -0600

    progress

--- src/lights.php
+++ src/lights.php
@@ -5,13 +5,13 @@ require_once 'header.php';
     <div class ="app" id="lights">
         <div class="header">
             <div id="left-light">
-                <a href="light-one.php"><i class="fas fa-lightbulb" <?php if(isset($_SESSION['light-one']) && $_SESSION['light-one'] == 1){ echo "id='current_page-i'"; } else { echo "id='nav-i'"; }?>></i></a>
+                <a href="light-one.php"><i class="fas fa-lightbulb" <?php if(isset($_SESSION['light-one']) && $_SESSION['light-one'] == 1){ echo "id='current_light-i'"; } else { echo "id='light-i'"; }?>></i></a>
             </div>
             <div id="middle-light">
-                <a href="light-two.php"><i class="fas fa-lightbulb" <?php if(isset($_SESSION['light-two']) && $_SESSION['light-two'] == 1){ echo "id='current_page-i'"; } else { echo "id='nav-i'"; }?>></i></a>
+                <a href="light-two.php"><i class="fas fa-sun" <?php if(isset($_SESSION['light-two']) && $_SESSION['light-two'] == 1){ echo "id='current_light-i'"; } else { echo "id='light-i'"; }?>></i></a>
             </div>
             <div class="right-light">
-                <a href="light-three.php"><i class="fas fa-lightbulb" <?php if(isset($_SESSION['light-three']) && $_SESSION['light-three'] == 1){ echo "id='current_page-i'"; } else { echo "id='nav-i'"; }?>></i></a>
+                <a href="light-three.php"><i class="fas fa-lightbulb" <?php if(isset($_SESSION['light-three']) && $_SESSION['light-three'] == 1){ echo "id='current_light-i'"; } else { echo "id='light-i'"; }?>></i></a>
             </div>
         </div>
     </div>
